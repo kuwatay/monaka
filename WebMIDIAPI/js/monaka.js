02a502a8dd2adb7741c2dae5c48cc5c7b7f48919
Yoshitaka Kuwata <kuwatay@nifty.com>
2015-08-01 14:34:40 +0900

    support analog/digital in

--- WebMIDIAPI/js/monaka.js
+++ WebMIDIAPI/js/monaka.js
@@ -14,6 +14,8 @@ var monaka = function() {
   this.timer = null;
   this.analogInputValues = [0,0,0,0];
   this.digitalInputValues = [0,0,0,0];
+  this.analogInEvent = null;
+  this.digitalInEvent = null;
 
   this.success = function(access){
     console.log("monaka.success()");
@@ -26,29 +28,45 @@ var monaka = function() {
     console.log("monaka.failure(): "+msg);
   }.bind(this);
 
+  this.setAnalogInEventCallback = function(func) {  // define a callback function for analog in
+    this.analogInEvent = func;
+  }
+
+  this.setDigitalInEventCallback = function(func) {  // define a callback function for digital in
+    this.digitalInEvent = func;
+  }
+  
   this.onMidiEvent = function(e){   // MIDIevent handler
     if (e.data[0] == 0xB0) {  // Control Change
       console.log("receive cc " + e.data[1] + " " + e.data[2]);
       if ((e.data[1] >= 22) && (e.data[1] <= 24)) {
-        this.analogInputValues[e.data[1] - 22] = e.data[3];
+        this.analogInputValues[e.data[1] - 22] = e.data[2];
+        if (this.analogInEvent != null) {  // callback analog in
+          this.analogInEvent(this.analogInputValues);
+        }
       }
     } else if (e.data[0] == 0x90) { // note on
       console.log("receive noteOn " + e.data[1] + " " + e.data[2]);
       if ((e.data[1] >= 60) && (e.data[1] <= 63)) {
         this.digitalInputValues[e.data[1] - 60] = e.data[2];
+        if (this.digitalInEvent != null) {  // callback digital in
+          this.digitalInEvent(this.digitalInputValues);
+        }
       }
     } else if (e.data[0] == 0x80) { // note off
       console.log("receive noteOff " + e.data[1] + " " + e.data[2]);
       if ((e.data[1] >= 60) && (e.data[1] <= 63)) {
         this.digitalInputValues[e.data[1] - 60] = 0;
+        if (this.digitalInEvent != null) { // callback digital in
+          this.digitalInEvent(this.digitalInputValues);
+        }
       }
     }
   }.bind(this);
 
   this.digitalOutput = function(pin, val) {
-    if (pin > 3) {
+    if (pin > 3) 
       return false;
-    }
     if (val == 0) {
       this.sendNoteOff(60 + pin, 0);
     } else {
@@ -58,27 +76,23 @@ var monaka = function() {
   }
 
   this.analogOutput = function(pin, val) {
-    if (pin > 3) {
+    if (pin > 3) 
       return false;
-    }
-    if (val > 127) {
+    if (val > 127)
       return false;
-    }
     this.sendControllerChange(channel, 22 + pin, val);
     return true;
   }
 
   this.digitalInput = function(pin) {
-    if (pin > 3) {
+    if (pin > 3)
       return false;
-    }
     return digitalInputValues[pin];
   }
 
   this.analogInput = function(pin) {
-    if (pin > 3) {
+    if (pin > 3)
       return false;
-    }
     return analogInputValues[pin];
   }
 
@@ -123,16 +137,14 @@ var monaka = function() {
     console.log("monaka.setMode(" + modeNo + ")");
     if ((modeNo <= 0) || (modeNo > 8))
        return;  // mode error
-    if (this.output != null) {
-      this.sendControlChange(122, modeNo);
-    }
+    if (this.output != null)
+      this.sendControlChange(122, modeNo);  // change mode of monaka
   }
 
   this.refreshPorts = function(){
     console.log("monaka.refreshPorts()");
     this.input = null;
     this.output = null;
-
     // input
     var it = this.midi.inputs.values();
     for(var o = it.next(); !o.done; o = it.next()){
@@ -143,7 +155,6 @@ var monaka = function() {
         break;
       }
     }
-
     // output
     var ot = this.midi.outputs.values();
     for(var o = ot.next(); !o.done; o = ot.next()){
@@ -153,7 +164,6 @@ var monaka = function() {
         break;
       }
     }
-
   }.bind(this);
 
   this.onConnect = function(e){
